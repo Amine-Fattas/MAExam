9b047ce8f5a45127a9d880ef2b84f8f4604e7d0e
Amine FATTAS <amine.fattas@adria-bt.com>
2022-05-17 13:24:53 +0100

    Convert static UsersViewModel to a UsersPresenter

--- MAExam/UsersVC/UsersVC.swift
+++ MAExam/UsersVC/UsersVC.swift
@@ -7,11 +7,29 @@
 
 import UIKit
 
-struct UserViewModel {
-    var users : [UserItem]
+class UsersPresenter {
+    var users : [UserItem] = []
+    var onUserSelection: (Int) -> Void
+    
+    init(onUserSelection: @escaping (Int) -> Void) {
+        self.onUserSelection = onUserSelection
+        load()
+    }
+    
+    func load(){
+        users = [
+            UserItem(id: 0, name: "a name", username: "an username", email: "an email"),
+            UserItem(id: 1, name: "a name", username: "an username", email: "an email"),
+            UserItem(id: 2, name: "a name", username: "an username", email: "an email"),
+            UserItem(id: 3, name: "a name", username: "an username", email: "an email"),
+            UserItem(id: 4, name: "a name", username: "an username", email: "an email"),
+            UserItem(id: 5, name: "a name", username: "an username", email: "an email"),
+        ]
+    }
 }
 
 struct UserItem{
+    let id: Int
     let name: String
     let username: String
     let email: String
@@ -24,7 +42,7 @@ class UsersVC: UIViewController {
     //MARK: - Properties -
     private let CELL_ID = "UserCell"
     
-    var viewModel = UserViewModel(users: [])
+    var presenter = UsersPresenter(onUserSelection: { print("Selected: ", $0)})
     
     //MARK: - init methods -
     override func viewDidLoad() {
@@ -50,17 +68,20 @@ class UsersVC: UIViewController {
 extension UsersVC : UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        self.viewModel.users.count
+        self.presenter.users.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: CELL_ID) as! UserCell
-        let user = viewModel.users[indexPath.row]
+        let user = presenter.users[indexPath.row]
         cell.ul_name.text = user.name
         cell.ul_username.text = user.username
         cell.ul_email.text = user.email
         return cell
     }
     
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        presenter.onUserSelection(indexPath.row)
+    }
 }
