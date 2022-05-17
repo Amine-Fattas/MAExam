b666f19172d0b9fc920b9844425584a30fea2bb6
Amine FATTAS <amine.fattas@adria-bt.com>
2022-05-17 12:46:57 +0100

    Update UsersVC: Implement UiTableView delegates

--- MAExam/UsersVC/UsersVC.swift
+++ MAExam/UsersVC/UsersVC.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class UsersVC: UIViewController {
+    
+    private let CELL_ID = "UserCell"
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        initUI()
+        initTableView()
+    }
+    
+    private func initTableView(){
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: CELL_ID, bundle: nil), forCellReuseIdentifier: CELL_ID)
+    }
+    
+    private func initUI(){
         title = "Users"
     }
 
 
+}
+
+extension UsersVC : UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CELL_ID) as! UserCell
+        return cell
+    }
+    
+    
 }
