501a2a55ea9b75ad52698e580cfb3776f0827cdd
Amine FATTAS <amine.fattas@adria-bt.com>
2022-05-17 11:16:08 +0100

    Initial Commit

---
+++ MAExam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAExam
+//
+//  Created by Amine Fattas on 17/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
